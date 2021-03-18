64d85155b937f2d64619d1ced50d8d9bae22bac5
nertc <davidtsiklauri7@gmail.com>
2021-03-18 16:33:23 +0400

    feat(fenwick_tree array): Finish Fenwick Tree consturctor for array inputs

--- fenwick_tree/main.cpp
+++ fenwick_tree/main.cpp
@@ -6,8 +6,7 @@ private:
     int _limit;
     std::vector<long long> _tree;
 
-public:
-    fenwick_tree(int limit)
+    void init(int limit)
     {
         _limit = limit;
 
@@ -15,29 +14,51 @@ public:
         _tree = tree;
     }
 
-    void add(long long index, long long value) {
-        if(index <= 0) return;
-        while(index <= _limit) {
+public:
+    fenwick_tree(int limit)
+    {
+        init(limit);
+    }
+
+    fenwick_tree(long long *arrBegin, long long *arrEnd)
+    {
+        init(arrEnd - arrBegin + 10);
+        for (int i = 0; arrBegin + i != arrEnd; ++i)
+        {
+            add(i + 1, *(arrBegin + i));
+        }
+    }
+
+    void add(long long index, long long value)
+    {
+        if (index <= 0)
+            return;
+        while (index <= _limit)
+        {
             _tree[index] += value;
             index += index & -index;
         }
     }
 
-    long long read(int index) {
+    long long read(int index)
+    {
         long long sum = 0;
-        while(index > 0) {
+        while (index > 0)
+        {
             sum += _tree[index];
             index -= index & -index;
         }
         return sum;
     }
 
-    void replace(int index, long long value) {
+    void replace(int index, long long value)
+    {
         long long addingValue = value - elementAt(index);
         add(index, addingValue);
     }
 
-    long long elementAt(int index) {
+    long long elementAt(int index)
+    {
         return read(index) - read(index - 1);
     }
 };
